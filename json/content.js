2a0d56d622b8bcf37ef409d8f23e97e2fa5a5698
Colin Legoedec <colin.legoedec@laposte.net>
2021-03-08 17:08:44 +0100

    Initial commit

---
+++ json/content.js
@@ -0,0 +1,5 @@
+const questAudios = ["sounds/questions/an.wav","sounds/questions/lay.wav","sounds/questions/nha.wav"] ;
+const idAudios =["an","lay","nha"];
+// on a donc questAudio[0] = chemin an ; questAudio[1] = chemin lay  ; questAudio[0] = chemin nha
+const corTexts = ["Bravo ! C'est la bonne réponse", "Ce n'est pas an c'est lay", "ce n’est pas nha c’est lay ", "ce n’est pas lay c’est an ","ce n’est pas nha c’est an","ce n’est pas an c’est nha ","ce n’est pas lay c’est nha" ];
+const corAudios = ["<audio id='bravo' src='sounds/bravo.wav'> </audio>","<audio id='an-lay' src='sounds/an-lay-c.wav'> </audio>","<audio id='nha-lay' src='sounds/nha-lay-c.wav'> </audio>","<audio id='lay-an' src='sounds/lay-an-c.wav'> </audio>","<audio id='nha-an' src='sounds/nha-an-c.wav'> </audio>","<audio id='an-nha' src='sounds/an-nha-c.wav'> </audio>","<audio id='lay-nha' src='sounds/lay-nha-c.wav'> </audio>"]
